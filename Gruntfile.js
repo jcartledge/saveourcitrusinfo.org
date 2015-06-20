2222392d6eb8ef395a0e2891b47f4c4be58035fb
James Cartledge <jcartledge@gmail.com>
2015-06-20 15:33:55 +1000

    less with autoprefix

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,56 @@
 module.exports = function(grunt) {
 
+  var Autoprefix = require('less-plugin-autoprefix');
+  var autoprefixPlugin = new Autoprefix({browsers: ["last 2 versions"]});
+
+  console.log(autoprefixPlugin);
+
   grunt.initConfig({
 
     jade: {
       compile: {
-        files: {
-          'build/index.html': ['src/index.jade']
+        files: [{
+          expand: true,
+          src: ["**/*.jade", "!**/_*.jade"],
+          dest: "build/",
+          cwd: "src/",
+          ext: '.html'
+        }]
+      }
+    },
+
+    less: {
+      compile: {
+        files: [{
+          expand: true,
+          src: ["**/*.less", "!**/_*.less"],
+          dest: "build/",
+          cwd: "src/",
+          ext: '.css'
+        }],
+        options: {
+          plugins: [autoprefixPlugin],
         }
       }
     },
 
     watch: {
-      files: ['src/**'],
-      tasks: ['build']
+      build: {
+        files: ['src/**'],
+        tasks: ['build']
+      },
+      grunt: {
+        files: ['Gruntfile.js']
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('build', ['jade:compile']);
+  grunt.registerTask('build', ['jade:compile', 'less:compile']);
   grunt.registerTask('default', ['build', 'watch']);
 
 };
+
